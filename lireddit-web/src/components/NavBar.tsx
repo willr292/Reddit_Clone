1aa5d4267934813d8eb38eefc06566dfd9cf097c
Will Rollason <willr292@gmail.com>
2021-04-07 19:17:39 +0100

    styling navbar

--- lireddit-web/src/components/NavBar.tsx
+++ lireddit-web/src/components/NavBar.tsx
@@ -46,13 +46,15 @@ export const NavBar = () => {
     );
   }
   return (
-    <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4} align="center">
-      <NextLink href="/">
-        <Link>
-          <Heading>Reddit</Heading>
-        </Link>
-      </NextLink>
-      <Box ml={"auto"}>{body}</Box>
+    <Flex zIndex={1} position="sticky" top={0} bg="orange" p={4}>
+      <Flex flex={1} m="auto" align="center" maxW={800}>
+        <NextLink href="/">
+          <Link>
+            <Heading>Reddit</Heading>
+          </Link>
+        </NextLink>
+        <Box ml={"auto"}>{body}</Box>
+      </Flex>
     </Flex>
   );
 };
